894cd103b5c5f7595fbd36b9aca4b64423487b21
QinVc <870459865@qq.com>
2023-08-21 19:02:57 +0800

    图片

--- GoodLuck/GLMain.cpp
+++ GoodLuck/GLMain.cpp
@@ -2,15 +2,16 @@
 #include <glfw3.h>
 #include <iostream>
 #include <string.h>
+#include <SOIL.h>
 #include "ShaderObject.h"
 
 using namespace std;
 
 GLfloat vertices[] = {
-0.5f, 0.5f, 0.0f,   // 右上角
-0.5f, -0.5f, 0.0f,  // 右下角
--0.5f, -0.5f, 0.0f, // 左下角
--0.5f, 0.5f, 0.0f   // 左上角
+0.5f, 0.5f, 0.0f, 1.0f, 1.0f,   // 右上角
+0.5f, -0.5f, 0.0f,1.0, 0.0f,  // 右下角
+-0.5f, -0.5f, 0.0f,0.0f,0.0f, // 左下角
+-0.5f, 0.5f, 0.0f,0.0f,1.0f  // 左上角
 };
 
 GLuint indices[] = { // 注意索引从0开始! 
@@ -56,7 +57,10 @@ void InitBuf(GLuint &VBO,GLuint &VAO, GLuint &EBO)
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(2*sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -65,7 +69,7 @@ void InitBuf(GLuint &VBO,GLuint &VAO, GLuint &EBO)
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
-	glEnableVertexAttribArray(0);
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
@@ -92,9 +96,27 @@ int main()
 	glViewport(0, 0, width, height);
 	glfwSetKeyCallback(window, key_callback);
 
-	ShaderObject FirstShader("C:/Users/Administrator/FirstShader.vs","C:/Users/Administrator/FirstShader.fs");
+	ShaderObject FirstShader("../Shader/FirstShader.vs","../Shader/FirstShader.fs");
 
 	InitBuf(VBO, VAO, EBO);
+
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture); // All upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// Set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load image, create texture and generate mipmaps
+	int imwidth, imheight;
+	unsigned char* image = SOIL_load_image("D:/UGit/LearnOpenGL/Content/awesomeface.png", &imwidth, &imheight, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imwidth, imheight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
 
 	//Not Work! EBO should Stay in GPU RAM
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -109,6 +131,7 @@ int main()
 
 		FirstShader.Use();
 
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
--- Shader/FirstShader.vs
+++ Shader/FirstShader.vs
@@ -1,10 +1,13 @@
 #version 330 core
 layout (location = 0) in vec3 position; // position变量的属性位置值为0
+layout (location = 1 ) in vec2 texCoord;
 
 out vec4 vertexColor; // 为片段着色器指定一个颜色输出
+out vec2 OurtexCoord;
 
 void main()
 {
     gl_Position = vec4(position, 1.0); // 注意我们如何把一个vec3作为vec4的构造器的参数
-    vertexColor = vec4(0.5f, 0.0f, 0.0f, 1.0f); // 把输出变量设置为暗红色
+    vertexColor = vec4(1f, 0.0f, 0.0f, 1.0f); // 把输出变量设置为暗红色
+    OurtexCoord=texCoord;
 }
